e89e5f79f49efc9ffa17e7e71d56edee1292bbb9
zhong <zhong@163.com>
2019-07-23 16:03:06 +0800

    角色修改和删除，用户删除

--- user/urls.py
+++ user/urls.py
@@ -4,12 +4,16 @@ from django.conf.urls import url
 
 urlpatterns = [
     path('verify_code/', views.verify_code),
-    path('', views.Login, name='login'),
     path('register/', views.register, name='register'),
     path('index/', views.index, name='index'),
     path('quit/', views.quit, name='quit'),
     path('role_index/', views.role_index, name='role_index'),
     path('role_jurisdiction/<int:sid>/', views.role_jurisdiction, name='role_jurisdiction'),
-    path('user_index/', views.user_index, name='user_index')
+    path('user_index/', views.user_index, name='user_index'),
+    path('user_deletes/',views.user_deletes, name='user_deletes'),
+    path('user_delete/<int:sid>/', views.user_delete, name='user_delete'),
+    path('user_editUser/<int:sid>/', views.user_editUser, name='user_editUser'),
+    path('password/<int:sid>',views.password,name='password'),
+    url('', views.Login, name='login')
 
 ]
